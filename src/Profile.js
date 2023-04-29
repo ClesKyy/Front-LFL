000ce60ba4eed3fff63f456366fc450b18b1426e
unknown <dacunha.clement@gmail.com>
2023-04-29 22:07:10 +0200

    card avatar

--- src/Profile.js
+++ src/Profile.js
@@ -1,17 +1,36 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import AppHeader from './AppHeader';
-import { Avatar, Card } from '@mui/material';
+import { Card, CardContent } from '@mui/material';
+import axios from 'axios';
 
 function Profile(props) {
+    const pseudo = localStorage.getItem("pseudo");
+    const [user, setUser] = useState({});
+    useEffect(() => {
+        axios.get(`https://localhost:7108/auth/${pseudo}`, {})
+            .then((res) => {
+                setUser(res.data);
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
     return (
         <div>
             <div>
                 <AppHeader />
             </div>
-            <Card>
-                <Avatar alt="Avatar" src="https://1.bp.blogspot.com/-9N4wHnisYnI/YTurIWCPi3I/AAAAAAAB_vE/x9UQiid2gwo1i1GJ_AOwrJyL-QDiTap-gCLcBGAsYHQ/w200-h200/5067.jpg" />
+            <Card className='profile-card'>
+                <div className='profile-avatar-div'>
+                    <img className='profile-avatar-icon' src="https://1.bp.blogspot.com/-9N4wHnisYnI/YTurIWCPi3I/AAAAAAAB_vE/x9UQiid2gwo1i1GJ_AOwrJyL-QDiTap-gCLcBGAsYHQ/w200-h200/5067.jpg" />
+                </div>
+                <div className='profile-user-name'>
+                   {user.pseudo}
+                </div>
+                <CardContent>Hello</CardContent>
             </Card>
         </div>
     );
