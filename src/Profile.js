318483003884f62244c8255088f039beacfdf815
unknown <dacunha.clement@gmail.com>
2023-04-30 18:26:30 +0200

    Profile page rank lvl

--- src/Profile.js
+++ src/Profile.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import AppHeader from './AppHeader';
-import { Card, CardContent } from '@mui/material';
+import { Card, CardContent, LinearProgress } from '@mui/material';
 import axios from 'axios';
 
 function Profile(props) {
@@ -25,12 +25,57 @@ function Profile(props) {
             </div>
             <Card className='profile-card'>
                 <div className='profile-avatar-div'>
-                    <img className='profile-avatar-icon' src="https://1.bp.blogspot.com/-9N4wHnisYnI/YTurIWCPi3I/AAAAAAAB_vE/x9UQiid2gwo1i1GJ_AOwrJyL-QDiTap-gCLcBGAsYHQ/w200-h200/5067.jpg" />
+                    <div>
+                        <img className='profile-avatar-icon' src="https://1.bp.blogspot.com/-9N4wHnisYnI/YTurIWCPi3I/AAAAAAAB_vE/x9UQiid2gwo1i1GJ_AOwrJyL-QDiTap-gCLcBGAsYHQ/w200-h200/5067.jpg" />
+                    </div>
+                    <div className='profile-avatar-level'>
+                        <span className='profile-avatar-level-number'>250</span>
+                    </div>
+
                 </div>
                 <div className='profile-user-name'>
-                   {user.pseudo}
+                    {user.pseudo}
                 </div>
-                <CardContent>Hello</CardContent>
+                <CardContent>
+                    <div className='profile-content'>
+                        <div className='profile-content-left'>
+                            <div className='profile-content-card-rank'>
+                                <div>
+                                    <img className='profile-content-card-rank-image' src='https://cdn3.emoji.gg/emojis/9476-challenger.png' />
+                                </div>
+                                <div>
+                                    <div style={{ fontWeight: 'bold', fontSize: '24px' }}>
+                                        Diamond
+                                    </div>
+                                    <div>
+                                        32 LP
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='profile-content-right'>
+                            <div className='profile-content-card'>
+                                <div className='profile-content-card-avatar-div'>
+                                    <img className='profile-content-card-avatar-image' src='https://1.bp.blogspot.com/-9N4wHnisYnI/YTurIWCPi3I/AAAAAAAB_vE/x9UQiid2gwo1i1GJ_AOwrJyL-QDiTap-gCLcBGAsYHQ/w200-h200/5067.jpg' />
+                                </div>
+                                <div className='profile-content-card-level'>
+                                    <div className='profile-content-card-level-number'>
+                                        <div style={{ fontWeight: 'bold', fontSize: '24px' }}>
+                                            Niv. 250
+                                        </div>
+                                        <div>
+                                            35 000 Exp
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <LinearProgress color='inherit' variant="determinate" value={10} />
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </CardContent>
             </Card>
         </div>
     );
