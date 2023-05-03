9a5f3b74bae6bfbf521ea11758e9ba4464489b4e
unknown <dacunha.clement@gmail.com>
2023-05-03 11:35:32 +0200

    Start Home Page

--- src/Home.js
+++ src/Home.js
@@ -5,15 +5,45 @@ import './App.css';
 
 function Home(props) {
     return (
-        <div>
-        <div>
-            <AppHeader />
-            
-        </div>
-        <div>
-            <img src='https://www.eclypsia.com/wp-content/uploads/eclypsia/2023/02/lfl-day-2023-1-1024x678.png'></img>
-        </div>
-        </div>
+        <body className='home-body'>
+            <div>
+                <div>
+                    <AppHeader />
+                </div>
+                <div className='home-header-banner'>
+                </div>
+                <div className='home-title-div'>
+                    <h1 className='home-title'>Suivez toutes la compétition française <br></br> de League of Legends </h1>
+                </div>
+                <div className='home-container-icon'>
+                    <div className='home-item-icon'>
+                        <div className='home-item-icon-content-image'>
+                        <img className='home-icon' src='https://cdn-icons-png.flaticon.com/512/661/661512.png'></img>
+                        </div>
+                        <div className='home-item-icon-content-text'>
+                            Ne ratez aucun match avec notre calendrier 
+                        </div>
+                    </div>
+                    <div className='home-item-icon'>
+                        <div className='home-item-icon-content-image'>
+                        <img className='home-icon' src='https://cdn-icons-png.flaticon.com/512/4301/4301534.png'></img>
+                        </div>
+                        <div className='home-item-icon-content-text'>
+                            Regarder qui est en sommet de la compétition 
+                        </div>
+                    </div>
+                    <div className='home-item-icon'>
+                        <div className='home-item-icon-content-image'>
+                        <img className='home-icon' src='https://cdn-icons-png.flaticon.com/512/3592/3592606.png'></img>
+                        </div>
+                        <div className='home-item-icon-content-text'>
+                            Parier en toute sécurité avec un système 100% gratuit &nbsp;
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </body>
     );
 }
 export default Home;
